f768d5606643d55876490308b5ff2f4ae594bddc
Elias Ferreira <elias.eliasferreira@gmail.com>
2020-03-13 13:14:17 -0300

    Initial Commit

---
+++ Teste2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Teste2
+//
+//  Created by Elias Ferreira on 13/03/20.
+//  Copyright © 2020 Elias Ferreira. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
